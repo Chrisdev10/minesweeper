79a8e86f41b59c726a9e323bd5a35623fdfee09c
Chrisdev10 <delbeke.christophe@hotmail.com>
2022-03-14 16:16:42 +0100

    fix settings width

--- src/app/guard/validator.settings.ts
+++ src/app/guard/validator.settings.ts
@@ -1,6 +1,6 @@
 import { AbstractControl, ValidationErrors, Validators } from "@angular/forms"
 export const INSERT_FORM_SETTINGS = {
-    'bomb': [12,[Validators.max(150)]],
-    'size': [10,[Validators.max(40)]]
+    'bomb': [12,[Validators.max(250)]],
+    'size': [10,[Validators.max(50)]]
 }
 
